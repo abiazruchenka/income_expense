0764bc2f4de13717405fe55094990edf0cebdf42
Anatol Biazruchanka <abiazruchanka@ampsoft.by>
2017-07-24 12:09:16 +0300

    add money format

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,6 +1,24 @@
+(function($) {
+    $.fn.currencyInput = function() {
+        this.each(function() {
+            var wrapper = $("<div class='currency-money-input' />");
+            $(this).wrap(wrapper);
+            $(this).before("<span class='currency-money-symbol'>$</span>");
+            $(this).change(function() {
+                var min = parseFloat($(this).attr("min"));
+                var value = this.valueAsNumber;
+                if(value < min)
+                    value = min;
+                $(this).val(value.toFixed(2));
+            });
+        });
+    };
+})(jQuery);
+
 $(document).ready(function() {
     $('#reset').on('click', function() {
         $('#select_category').val('0');
         $( "input[name*='datepicker']" ).val( "" );
     })
+    $('input.currency-money').currencyInput();
 });
