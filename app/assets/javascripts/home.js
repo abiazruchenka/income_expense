bbea5e4ffa83f4d3d20f8a1a3029f81e0ac0e221
Anatol Biazruchanka <abiazruchanka@ampsoft.by>
2017-07-24 12:35:58 +0300

    fixing turbolinks

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,23 +1,30 @@
+$(document).ready(function() {
+    $('#reset').on('click', function() {
+        $('#select_category').val('0');
+        $( "input[name*='datepicker']" ).val( "" );
+    })
+});
+
 (function($) {
     $.fn.currencyInput = function() {
         this.each(function() {
-            var wrapper = $("<div class='currency-money-input' />");
+            var wrapper = $("<div class='currency-input' />");
             $(this).wrap(wrapper);
-            $(this).before("<span class='currency-money-symbol'>$</span>");
+            $(this).before("<span class='currency-symbol'>$</span>");
             $(this).change(function() {
                 var min = parseFloat($(this).attr("min"));
+                var max = parseFloat($(this).attr("max"));
                 var value = this.valueAsNumber;
                 if(value < min)
                     value = min;
+                else if(value > max)
+                    value = max;
                 $(this).val(value.toFixed(2));
             });
         });
     };
 })(jQuery);
-$(document).on('turbolinks:load', function() {
-    $('#reset').on('click', function() {
-        $('#select_category').val('0');
-        $( "input[name*='datepicker']" ).val( "" );
-    })
-    $('input.currency-money').currencyInput();
-})
+
+$(document).ready(function() {
+    $('input.currency').currencyInput();
+});
