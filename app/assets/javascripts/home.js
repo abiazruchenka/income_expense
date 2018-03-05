315ef317921b8fc4cc2c37eda0f86cfc76ceb236
Anatol Bezruchenko <abezruchenko@beltechsoftware.com>
2018-03-05 12:31:52 +0300

    #95878 test message master 1

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('#reset').on('click', function() {
         $('#select_category').val('0');
-        $( "input[name*='datepicker']" ).val( "" );
+        $( "input[name*= 'datepicker']" ).val( "" );
     })
 });
 
